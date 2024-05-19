fb7a80d8697e00c49b43fe64ae220096c6766c55
Jahnavi-dev <jahnavi9322@gmail.com>
2024-05-19 15:40:04 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,81 +1,11 @@
 import {React} from "react";
-import Button2Comp from "./Components/functional/Button/button2";
-import { Para2Comp } from "./Components/functional/Paragraph/paragraph2";
-import Para3Comp from "./Components/functional/Paragraph/paragraph3";
-import Counter1Comp from "./Components/Class/Counter/counter1";
-import Counter2Comp from "./Components/Class/Counter/counter2";
+import Products from "./Components/Class/Products/products";
 
 
-//prop and state practice 
-//prop and state practice 
-//prop and state practice 
 
 
 const App=()=>{ 
-  const buttons=[
-    {id:1,
-      text:"click me",
-      height:100,
-      width:100,
-      bgcolor:"green",
-      color:"blue"
-    },
-    {id:2,
-      text:"Sign in",
-      height:100,
-      width:100,
-      bgcolor:"pink",
-      color:"violet"
-    },
-    {id:3,
-      text:"login me",
-      height:100,
-      width:100,
-      bgcolor:"black",
-      color:"white"
-    },
-    {id:4,
-      text:"logout",
-      height:100,
-      width:100,
-      bgcolor:"yellow",
-      color:"green"
-    }
-  ]
-  
-  return(
-    <>
-    {buttons.map((eachButton)=>{
-      const {id,text, height, width, bgcolor, color}=eachButton
-      return(<>
-      <Button2Comp 
-      key={id} 
-      text={text} 
-      bgColor={bgcolor} 
-      buttonProperties={{color:color, height:height, width:width}}/>
-      </>)
-    })}
-
-
-    <Para2Comp>
-      <h1>Children Props in parent</h1>
-      <p>Good Morning</p>
-      <p>Good Evening</p>
-    </Para2Comp>
-    <Para3Comp>
-      <>
-      <h1>Flowers</h1>
-      </>
-    </Para3Comp>
-    
-
-    <Counter1Comp></Counter1Comp>
-    <br/>
-    <br/>
-    
-    <Counter2Comp></Counter2Comp>
-    </>
-  )
+  <Products></Products>
 }
 
 export default App
---
+++ src/Components/Class/Products/products.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+class Products extends Component{
+    products=[
+        {
+            productName:"Laptop1",
+        },
+        {
+            productName:"Laptop2",
+        },
+        {
+            productName:"Laptop3",
+        },
+        {
+            productName:"Laptop4",
+        }
+    ]
+    state={
+        quantity1:0,
+        quantity2:0,
+        quantity3:0,
+        quantity4:0
+
+    }
+    
+    render(){
+        return(<></>)
+    }
+
+}
+
+export default Products
