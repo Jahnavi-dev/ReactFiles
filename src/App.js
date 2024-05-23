5a8c6509583c540d2679c40ca79560220a643159
Jahnavi-dev <jahnavi9322@gmail.com>
2024-05-23 17:35:41 +0530

    7class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react"
-import CategoryWiseList from "./Components/Class/Category/category"
-import { Spinner1Comp } from "./Components/functional/Spinner/spinner1"
+
 
 
 
@@ -11,8 +10,7 @@ import { Spinner1Comp } from "./Components/functional/Spinner/spinner1"
 const App=()=>{ 
   return(
     <React.Fragment>
-      <h3 style={{margin:"10px"}}>View All the Products by filtering CategoryWise</h3>
-      <CategoryWiseList/>
+      
     </React.Fragment>
   )
 }
